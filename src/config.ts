7920d21856ee2c42a0d646229ea28f1b2151aec3
kanesea2542 <kanesea2542@gmail.com>
2026-01-09 00:29:56 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,11 @@
 export const siteConfig = {
-  name: "Ryan Fitzgerald",
-  title: "Senior Software Engineer",
-  description: "Portfolio website of Ryan Fitzgerald",
-  accentColor: "#1d4ed8",
+  name: "Sean Kane",
+  title: "Electrical Engineering - Stony Brook University",
+  description: "Portfolio website of Sean Kane",
+  accentColor: "#00555A",
   social: {
-    email: "your-email@example.com",
-    linkedin: "https://linkedin.com/in/yourprofile",
-    twitter: "https://x.com/rfitzio",
-    github: "https://github.com/RyanFitzgerald",
+    email: "sean.ryan.kane@gmail.com",
+    github: "https://github.com/kanesea2542",
   },
   aboutMe:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
